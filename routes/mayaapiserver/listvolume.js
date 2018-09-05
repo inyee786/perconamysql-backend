d9be4a5b4a1de3b71215be73c9316d9ede027271
inyee786 <intakhab.cusat@gmail.com>
2018-09-06 00:33:38 +0530

    add promise for mayalist volume

--- routes/mayaapiserver/listvolume.js
+++ routes/mayaapiserver/listvolume.js
@@ -10,62 +10,77 @@ var options = {
     method: 'GET',
     headers: {
         'Content-Type': 'application/json',
-        'namespace' : 'percona-jiva'
+        'namespace': `${process.argv[4]}`
     }
 };
 
 http.get(options, function (err, resp, body) {
-    var volumeStatus =[];
+    var volumeStatus = [];
     if (err) {
         console.log("this is volume erro namespaces ");
     } else {
         data = JSON.parse(body);
         console.log(JSON.stringify(data.items));
-        console.log("this is volume lis http");     
+        console.log("this is volume lis http");
 
-        for(i=0;i<data.items.lenght;i++){
+        for (i = 0; i < data.items.lenght; i++) {
             volumeStatus.push({
                 volumeName: data.items[i].metadata.name,
                 controllerStatus: data.items[i].metadata.annotations['vsm.openebs.io/controller-status'],
                 replicaCount: data.items[i].metadata.annotations['vsm.openebs.io/replica-count'],
-                replicaStatus:data.items[i].metadata.annotations['vsm.openebs.io/replica-status'],
-                capacity:data.items[i].metadata.annotations['vsm.openebs.io/volume-size'],
+                replicaStatus: data.items[i].metadata.annotations['vsm.openebs.io/replica-status'],
+                capacity: data.items[i].metadata.annotations['vsm.openebs.io/volume-size'],
             });
         }
-        console.log(volumeStatus);
-    }    
+    }
 });
 
-router.get('/volume', (req, res) => {
-    var volumeStatus =[];
+function getvolume() {
     var options = {
         url: `http://${process.argv[5]}:5656/latest/volumes/`,
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
-            'namespace' : `${process.argv[4]}`
+            'namespace': `${process.argv[4]}`
         }
     };
+    return new Promise(function (resolve, reject) {
+        http.get(options, function (err, resp, body) {
+            var volumeStatus = [];
+            if (err) {
+                console.log("this is volume erro namespaces ");
+            } else {
+                data = JSON.parse(body);
+                console.log(JSON.stringify(data.items));
+                console.log("this is volume lis http");
 
-    http.get(options, function (err, resp, body) {
-        if (err) {
-            //   reject(err);
-            console.log(err);
-            console.log("this is volume erro namespaces ");
-        } else {
-            data = JSON.parse(body);
-            console.log(JSON.stringify(data.items));
-            for(i=0;i<data.items.lenght;i++){
-                volumeStatus.push({
-                    volumeName: data.items[i].metadata.name,
-                    controllerStatus: data.items[i].metadata.annotations['vsm.openebs.io/controller-status'],
-                    replicaCount: data.items[i].metadata.annotations['vsm.openebs.io/replica-count'],
-                    replicaStatus:data.items[i].metadata.annotations['vsm.openebs.io/replica-status'],
-                    capacity:data.items[i].metadata.annotations['vsm.openebs.io/volume-size'],
-                });
+                for (i = 0; i < data.items.lenght; i++) {
+                    volumeStatus.push({
+                        volumeName: data.items[i].metadata.name,
+                        controllerStatus: data.items[i].metadata.annotations['vsm.openebs.io/controller-status'],
+                        replicaCount: data.items[i].metadata.annotations['vsm.openebs.io/replica-count'],
+                        replicaStatus: data.items[i].metadata.annotations['vsm.openebs.io/replica-status'],
+                        capacity: data.items[i].metadata.annotations['vsm.openebs.io/volume-size'],
+                    });
+                }
+                console.log(volumeStatus);
+                resolve(volumeStatus);
             }
-            res.status(200).json({ volumeStatus });
-        }     
+        });
+    });
+}
+
+
+
+getvolume().then(function (result) {
+    console.log(result);
+});
+
+
+router.get('/volume', (req, res) => {
+    getvolume().then(function (result) {
+        console.log(result);
+        res.status(200).json({ volumeStatus });
     });
 
 });
