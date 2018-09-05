d5b5be100db1414fcb2b51d00693d0de87a53ac3
inyee786 <intakhab.cusat@gmail.com>
2018-09-05 23:52:51 +0530

    api foropenebs  volume

--- routes/mayaapiserver/listvolume.js
+++ routes/mayaapiserver/listvolume.js
@@ -2,23 +2,8 @@ const express = require("express");
 const router = express();
 const http = require('request');
 
-var options = {
-    url: `http://${process.argv[5]}:5656/latest/volumes/`,
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-    }
-};
 
-http.get(options, function (err, resp, body) {
-    if (err) {
-        console.log("this is volume erro namespaces ");
-    } else {
-        data = JSON.parse(body);
-        console.log(JSON.stringify(data));
-        console.log("this is volume lis http");     
-    }    
-});
+
 
 var options = {
     url: `http://${process.argv[5]}:5656/latest/volumes/`,
@@ -30,22 +15,35 @@ var options = {
 };
 
 http.get(options, function (err, resp, body) {
+    var volumeStatus =[];
     if (err) {
         console.log("this is volume erro namespaces ");
     } else {
         data = JSON.parse(body);
-        console.log(JSON.stringify(data));
+        console.log(JSON.stringify(data.items));
         console.log("this is volume lis http");     
+
+        for(i=0;i<data.items.lenght;i++){
+            volumeStatus.push({
+                volumeName: data.items[i].metadata.name,
+                controllerStatus: data.items[i].metadata.annotations['vsm.openebs.io/controller-status'],
+                replicaCount: data.items[i].metadata.annotations['vsm.openebs.io/replica-count'],
+                replicaStatus:data.items[i].metadata.annotations['vsm.openebs.io/replica-status'],
+                capacity:data.items[i].metadata.annotations['vsm.openebs.io/volume-size'],
+            });
+        }
+
     }    
 });
 
 router.get('/volume', (req, res) => {
-
+    var volumeStatus =[];
     var options = {
         url: `http://${process.argv[5]}:5656/latest/volumes/`,
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
+            'namespace' : `${process.argv[4]}`
         }
     };
 
@@ -56,14 +54,18 @@ router.get('/volume', (req, res) => {
             console.log("this is volume erro namespaces ");
         } else {
             data = JSON.parse(body);
-            console.log(data);
-            //   numberOfrepo = JSON.parse(body).length;
-            console.log("this is volume lis http");
-            //    console.log(resp);
-            console.log("this is volume lis http");
-        }
-        // console.log(body +' this is body1')
-        res.status(200).json({ data });
+            console.log(JSON.stringify(data.items));
+            for(i=0;i<data.items.lenght;i++){
+                volumeStatus.push({
+                    volumeName: data.items[i].metadata.name,
+                    controllerStatus: data.items[i].metadata.annotations['vsm.openebs.io/controller-status'],
+                    replicaCount: data.items[i].metadata.annotations['vsm.openebs.io/replica-count'],
+                    replicaStatus:data.items[i].metadata.annotations['vsm.openebs.io/replica-status'],
+                    capacity:data.items[i].metadata.annotations['vsm.openebs.io/volume-size'],
+                });
+            }
+            res.status(200).json({ volumeStatus });
+        }     
     });
 
 });
