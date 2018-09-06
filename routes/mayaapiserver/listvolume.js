df0b38e747dcb0c73ba8ba4107b76d8fdbc9bc78
inyee786 <intakhab.cusat@gmail.com>
2018-09-06 08:12:17 +0530

    list jiva volume detaills

--- routes/mayaapiserver/listvolume.js
+++ routes/mayaapiserver/listvolume.js
@@ -25,27 +25,33 @@ http.get(options, function (err, resp, body) {
     }
 });
 
-router.get('/volume', (req, res) => {
-            var volumeStatus = [];
-            var options = {
-                url: `http://${process.argv[5]}:5656/latest/volumes/`,
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'namespace': `${process.argv[4]}`
-                }
-            };
-            http.get(options, function (err, resp, body) {
-                if (err) {
-                    //   reject(err);
-                    console.log(err);
-                    console.log("this is volume erro namespaces ");
-                } else {
-                    data = JSON.parse(body);
-                    console.log(JSON.stringify(data.items));
-                    res.status(200).json({ data });
-                }
-            });
+app.get('/volume', (req, res) => {
 
-        });
-    module.exports = router;
+    var options = {
+        url: `http://${process.argv[5]}:5656/latest/volumes/`,
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'namespace': `${process.argv[4]}`
+        }
+    };
+
+    http.get(options, function (err, resp, body) {
+        if (err) {
+            //   reject(err);
+            console.log(err);
+            console.log("this is volume erro namespaces ");
+        } else {
+            data = JSON.parse(body);
+            console.log(data);
+            //   numberOfrepo = JSON.parse(body).length;
+            console.log("this is volume lis http");
+            //    console.log(resp);
+            console.log("this is volume lis http");
+        }
+        // console.log(body +' this is body1')
+        res.status(200).json({ data });
+    });
+
+});
+module.exports = router;
